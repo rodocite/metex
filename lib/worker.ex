a80f1c6ab851436cb96ae9356a061328a2c73389
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-15 09:05:56 -0700

    Enable worker to listen to messages

--- lib/worker.ex
+++ lib/worker.ex
@@ -2,6 +2,16 @@ defmodule Metex.Worker do
   
   @apikey "b03676716ea37864a16cb2af011b1376"
   
+  def loop do
+    receive do
+      {sender_pid, location} ->
+        send(sender_pid, {:ok, temperature_of(location)})
+      _ -> IO.puts("Don't know how to process this message.")
+    end
+
+    loop()
+  end
+
   def temperature_of(location) do
     url_for(location) 
     |> HTTPoison.get 
@@ -38,3 +48,10 @@ defmodule Metex.Worker do
     end
   end
 end
+
+# cities = ["Los Angeles", "San Francisco", "New York", "Shanghai", "Seoul"]
+
+# cities |> Enum.each(fn city -> 
+#   pid = spawn(Metex.Worker, :loop, [])
+#   send(pid, {self, city})
+# end)
