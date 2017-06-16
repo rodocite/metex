c21b4649b9a801e54ad2cf9ed0157681fc3d23a8
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-15 20:45:44 -0700

    Create coordinator
    Opted not to use conditionals within functions and break the conditions
    out into pattern matched private functions.

---
+++ lib/coordinator.ex
@@ -0,0 +1,31 @@
+defmodule Metex.Coordinator do
+  
+  def loop(results \\ [], results_expected) do
+    receive do
+      {:ok, result} ->
+        handle_loop(result, results, results_expected)
+      :exit ->
+        handle_loop(:exit, results)
+      _ ->
+        handle_loop(results, results_expected)
+    end
+  end
+
+  defp handle_loop(result, results, results_expected) do
+    new_results = [result | results]
+
+    all_results_resolved?(results_expected, new_results)
+
+    loop(new_results, results_expected)
+  end
+
+  defp handle_loop(:exit, results) do
+    IO.puts(results |> Enum.sort |> Enum.join(", "))
+  end
+
+  defp handle_loop(results, results_expected) do
+    loop(results, results_expected)
+  end
+
+  defp all_results_resolved?(expected, new) when expected == :erlang.length(new), do: send(self, :exit)
+end
