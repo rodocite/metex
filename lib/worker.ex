781b756b2fdd0bbd7933d903e9ff97897e60f168
Rodolfo Yabut <rodolfoyabut@gmail.com>
2017-06-17 06:09:41 -0700

    Singleton GenServer process registration

--- lib/worker.ex
+++ lib/worker.ex
@@ -1,27 +1,28 @@
 defmodule Metex.Worker do
   use GenServer
 
+  @name Apricot
   @apikey "b03676716ea37864a16cb2af011b1376"
   
   ## Client API
   def start_link(opts \\ []) do
-    GenServer.start_link(__MODULE__, :ok, opts)
+    GenServer.start_link(__MODULE__, :ok, opts ++ [name: Apricot])
   end
 
-  def get_temperature(pid, location) do
-    GenServer.call(pid, {:location, location})
+  def get_temperature(location) do
+    GenServer.call(@name, {:location, location})
   end
 
-  def get_stats(pid) do
-    GenServer.call(pid, :get_stats)
+  def get_stats do
+    GenServer.call(@name, :get_stats)
   end
 
-  def reset_stats(pid) do
-    GenServer.cast(pid, :reset_stats)
+  def reset_stats do
+    GenServer.cast(@name, :reset_stats)
   end
 
-  def stop(pid) do
-    GenServer.cast(pid, :stop)
+  def stop do
+    GenServer.cast(@name, :stop)
   end
 
   ## Callbacks
